3fb57e2bb049dc622e03ce5c5e2862ec10dd8978
seanmun <smunley13@gmail.com>
2025-03-25 22:33:32 -0400

    case error

--- src/pages/learn/LightswitchModule.tsx
+++ src/pages/learn/LightswitchModule.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import PageLayout from '../../components/layout/PageLayout';
 import LightswitchIntro from '../../pages/learn/lightswitch/LightswitchIntro';
 import LightswitchExample from '../../pages/learn/lightswitch/LightswitchExample';
-import LightswitchBestPractices from '../../pages/learn/lightswitch/LightswitchBestPractices';
+import LightswitchBestPractices from './lightswitch/LightswitchBestPractices';
 
 const LightswitchModule: React.FC = () => {
   return (
