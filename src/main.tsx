4d74d9a0f815e6a9779b768cab3ecbe398e8bb87
seanmun <smunley13@gmail.com>
2025-07-08 22:03:27 -0400

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import router from './routes';
-import './styles/tailwind.css'; // This import is crucial
+import './styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>,
 );
